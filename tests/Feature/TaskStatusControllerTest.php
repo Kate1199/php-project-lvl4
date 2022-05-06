cae62bab1b1ad7106e6b813c73136bfd9ea74cf0
Kate1199 <kate.kisel99@yandex.by>
2022-05-06 23:56:58 +0300

    create task status tests

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -15,10 +15,53 @@ class TaskStatusTest extends TestCase
         parent::setUp();
         $this->seed();
     }
+
     public function testCreate(): void
     {
         $response = $this->get(route('task_statuses.create'));
 
         $response->assertOk();
+    }
+
+    public function testStore(): void
+    {
+        $taskStatus = TaskStatus::factory()->make();
+
+        $this->post(route('task_statuses.store'), $taskStatus);
+
+        $this->assertDatabaseHas('task_statuses', $taskStatus);
+    }
+
+    public function testShow(): void
+    {
+        $response1 = $this->get(route('task_statuses.show', ['task_status' => 1]));
+        $response1->assertOk();
+
+        $response2 = $this->get(route('task_statuses.show', ['task_status' => 2]));
+        $response2->assertOk();
+
+        $response3 = $this->get(route('task_statuses.show', ['task_status' => 3]));
+        $response3->assertOk();
+    }
+
+    public function testEdit(): void
+    {
+        $response = $this->get(route('task_statuses.edit', ['task_status' => 1]));
+
+        $response->assertOk();
+    }
+
+    public function testUpdate(): void
+    {
+        $editedTaskStatus = TaskStauts::factory()->make();
+
+        $this->patch(route('task_statuses.update', ['task_status' => 1]), $editedTaskStatus);
+
+        $this->assertDatabaseHas('task_statuses', $editedTaskStatus);
+    }
+
+    public function testDestroy(): void
+    {
+        
     }
 }
