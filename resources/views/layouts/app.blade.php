5bb7cdf26de9c27b43f39a6c3ffcdd4446b3f14f
Kate1199 <kate.kisel99@yandex.by>
2022-05-19 19:29:54 +0300

    add tasks to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,14 +35,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a href="{{ route('task_statuses.index') }}" class="nav-link">
-                                {{ __('layouts.statuses') }}
-                            </a>
-                        </li>
                         <li>
                             <a href="{{ route('tasks.index') }}" class="nav-link">
                                 {{ __('layouts.tasks')}}
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('task_statuses.index') }}" class="nav-link">
+                                {{ __('layouts.statuses') }}
                             </a>
                         </li>
                     </ul>
