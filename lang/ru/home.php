b4fefb48ccd9ce49cdb1d2a9ccd2aa00972e4ca1
Kate1199 <kate.kisel99@yandex.by>
2022-05-29 20:44:26 +0300

    fix home page

---
+++ lang/ru/home.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'header' => 'Привет от Хекслета!',
+    'subheader' => 'Практические курсы по программированию',
+    'hexlet' => 'https://ru.hexlet.io',
+    'learn_more' => 'Узнать больше',
+];
