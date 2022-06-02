b2718892cca4404d109e0cacabf42d317d7ca391
Kate1199 <kate.kisel99@yandex.by>
2022-06-02 13:38:54 +0300

    fix email verification

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Auth::routes(['verify' => false]);
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
