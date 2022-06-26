ac5d75757da7e8ce895789e3bcfce342bbfb2ff9
Kate1199 <kate.kisel99@yandex.by>
2022-06-26 22:54:35 +0300

    fix attributes for form

--- resources/views/task_status/form.blade.php
+++ resources/views/task_status/form.blade.php
@@ -2,5 +2,5 @@
 
 <div class="form-group mb-3">
     {{ Form::label('name', __('task_status.name')) }}
-    {{ Form::text('name', $taskStatus->name, ['class' => 'form-control']) }}
+    {{ Form::text('name', $taskStatus->name, ['class' => 'form-control', 'required']) }}
 </div>
